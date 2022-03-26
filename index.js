38b9a34523fb9259888e6cd21425483324809c57
jstirpe <jennstirpe@yahoo.com>
2022-03-25 22:30:25 -0400

    Added rating functionality

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-//  .option:active {
-//     background-color: $orange;
-//     color: $white;
-// }
+
+const options = document.querySelectorAll(".option");
+const submit = document.querySelector("#submit");
+const rating = document.querySelector("#rating");
+const ratingCard = document.querySelector("#rating-card");
+const confirmCard = document.querySelector("#confirmation-card");
+
+let ratingValue;
+
+
+// selecting rating
+for(let option of options) {
+    option.addEventListener("click", () => {
+        
+        options.forEach(option => option.classList.remove("active"));
+        option.classList.add("active");
+
+        ratingValue = option.value;
+    })
+};
+
+// submitting rating
+submit.addEventListener("click", () => {
+
+    rating.innerText = ratingValue;
+
+    if(ratingValue) {
+        ratingCard.style.opacity = "0";
+        ratingCard.style.zIndex = "0";
+        confirmCard.style.opacity = "1";
+        confirmCard.style.zIndex = "1";
+    } else {
+        alert("Please select a rating!")
+    }
+
+});
